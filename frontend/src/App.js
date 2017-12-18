fbcbf6cbbb570536ff85baee9935d3429e262baa
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2017-12-18 17:51:40 +0700

    Logging in the chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,60 @@
 import React, { Component } from "react";
 import openSocket from "socket.io-client";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    nickname: localStorage.getItem("nickname"),
+    hasNickName: localStorage.getItem("nickname") ? true : false,
+    connected: false
+  };
   componentDidMount() {
-    this.socket = openSocket("http://localhost:3000");
+    const { hasNickName } = this.state;
+    if (hasNickName) {
+      this._logUserIn();
+    }
   }
   render() {
+    const { nickname, hasNickName } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {!hasNickName && (
+          <form onSubmit={this._submit}>
+            <input
+              type="text"
+              value={nickname || ""}
+              onChange={this._updateNickname}
+            />
+          </form>
+        )}
       </div>
     );
   }
+  _submit = e => {
+    e.preventDefault();
+    const { nickname } = this.state;
+    localStorage.setItem("nickname", nickname);
+    this.setState({
+      hasNickName: true
+    });
+    this._logUserIn();
+  };
+  _updateNickname = e => {
+    const { target: { value } } = e;
+    this.setState({
+      nickname: value
+    });
+  };
+  _logUserIn = () => {
+    const { nickname } = this.state;
+    this.socket = openSocket("http://localhost:3000");
+    this.socket.on("connect", () => {
+      this.setState({
+        connected: true
+      });
+      this.socket.emit("login", nickname);
+    });
+  };
 }
 
 export default App;
